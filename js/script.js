7462bce0135beeb30b8689ae29aaa89e64cd7d30
Frontend <shomilisayevich707@gmail.com>
2024-05-09 13:52:37 +0500

    blogs

--- js/script.js
+++ js/script.js
@@ -97,122 +97,62 @@ $(document).ready(function(){
         
     });
 
-    // Attaching the event listener function to window's resize event
-    // window.addEventListener("resize", displayWindowSize);
-    
-    // // Calling the function for the first time
-    // displayWindowSize();
+    $(".projects-slider").slick({
+        arrows: true,
+        dots:false,
+        slidesToShow:2,
+        // slidesToScroll:1,
+        infinite:false,
+        centerMode: false,
+        virableWidth:false, 
+        responsive: [
+            // {
+            //     breakpoint: 1430,
+            //     settings: {
+            //         slidesToShow:4,
+            //     },
 
-    
-    
+               
+            // },
+            // {
+            //     breakpoint: 1280,
+            //     settings: {
+            //         slidesToShow:3,
+            //     },
+
+               
+            // },
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow:1,
+                },
+
+               
+            },
+            {
+                breakpoint: 495,
+                settings: {
+                    slidesToShow:1,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    arrows: false,
+                },
+
+               
+            },
+
+        ],
+        
+        
+    });
+
+   
     
 
 });
 
 
 
-
-function displayWindowSize(){
-    // Get width and height of the window excluding scrollbars
-    var w = document.documentElement.clientWidth;
-    var h = document.documentElement.clientHeight;
-    if(w <= 1430) {
-
-        $(".projects-slider").slick({
-   
-            responsive: [
-                {
-                    breakpoint: 1430,
-                    settings: {
-                        slidesToShow:2,
-                        
-                    },
-    
-                   
-                },
-                // {
-                //     breakpoint: 1280,
-                //     settings: {
-                //         slidesToShow:3,
-                //     },
-    
-                   
-                // },
-                {
-                    breakpoint: 1000,
-                    settings: {
-                        slidesToShow:1,
-                        
-                    },
-    
-                   
-                },
-                // {
-                //     breakpoint: 700,
-                //     settings: {
-                //         slidesToShow:1,
-                //     },
-    
-                   
-                // },
-    
-            ],
-            
-            
-        });
-
-    } 
-    
-}
-
-function execs(){
-    if(screen.width <= 1430) {
-
-        $(".projects-slider").slick({
-   
-            responsive: [
-                {
-                    breakpoint: 1430,
-                    settings: {
-                        slidesToShow:2,
-                        
-                    },
-    
-                   
-                },
-                // {
-                //     breakpoint: 1280,
-                //     settings: {
-                //         slidesToShow:3,
-                //     },
-    
-                   
-                // },
-                {
-                    breakpoint: 1000,
-                    settings: {
-                        slidesToShow:1,
-                        
-                    },
-    
-                   
-                },
-                // {
-                //     breakpoint: 700,
-                //     settings: {
-                //         slidesToShow:1,
-                //     },
-    
-                   
-                // },
-    
-            ],
-            
-            
-        });
-
-      }
- 
-}
 
 
